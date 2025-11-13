517cb23596f6b780ead37a7322ff5aea7db851e3
KocTblJLb-oO <x07.05.18@yandex.ru>
2025-11-13 12:00:39 +0300

    Diplom. Stats. 2511131228

---
+++ stats-service/stats-server/src/main/java/ru/practicum/ewm/controller/StatsController.java
@@ -0,0 +1,37 @@
+package ru.practicum.ewm.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.ewm.dto.EndpointHitDto;
+import ru.practicum.ewm.dto.ViewStats;
+import ru.practicum.ewm.service.StatsService;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class StatsController {
+    private final StatsService statsService;
+
+    @PostMapping("/hit")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveHit(@RequestBody EndpointHitDto endpointHitDto) {
+        log.info("Запрос для saveHit {}", endpointHitDto);
+        statsService.saveHit(endpointHitDto);
+    }
+
+    @GetMapping("/stats")
+    public List<ViewStats> getStats(
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
+            @RequestParam(required = false) List<String> uris,
+            @RequestParam(defaultValue = "false") boolean unique) {
+
+        return statsService.getStats(start, end, uris, unique);
+    }
+}
